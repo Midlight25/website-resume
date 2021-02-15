e200d28c553c44fef53859991b20a92d517f1ac0
Michael Mesquita <mannolaur@gmail.com>
2021-02-15 12:28:08 -0500

    Got the navbar toggler working

--- src/index.js
+++ src/index.js
@@ -2,8 +2,3 @@
 const bootstrap = require('bootstrap');
 
 console.log("Modules Loaded");
-
-let exampleEl = document.getElementById('exampleTooltip');
-let tooltip = new bootstrap.Tooltip(exampleEl, {
-	animation: true,
-})
