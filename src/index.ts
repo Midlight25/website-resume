eb94cbb5574b80fbdf73a704b0378b747ad44fed
Michael Mesquita <mannolaur@gmail.com>
2021-03-21 17:01:36 -0400

    Updated dependencies

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@ import { Collapse } from 'bootstrap';
 
 console.log("Modules Loaded");
 
-// 
+// Enabling the navbar collapse element.
 let collapseElement = document.querySelector(".collapse");
 let collapseEntity = new Collapse(collapseElement);
