6c42e2805bb1fec7b9857da1844005905fe94dc8
Michael Mesquita <mannolaur@gmail.com>
2021-03-24 17:26:25 -0400

    Added ScrollSpying to website.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 'use strict;'
 
-// Importing Bootstrap Collapse.
-// I'm not using any other components.
-import { Collapse } from 'bootstrap';
+// Importing Bootstrap Collapse and ScrollSpy
+// Tree-shaking!
+import { Collapse, ScrollSpy } from 'bootstrap';
 
 console.log("Modules Loaded");
 
@@ -12,4 +12,9 @@ let collapseList:Array<Collapse> = collapseElementList.map(function (collapseEl)
 	return new Collapse(collapseEl, {
 		toggle: false
 	});
+});
+
+// Enabling ScrollSpy
+let scrollSpyElements:ScrollSpy = new ScrollSpy(document.body, {
+	target: '#navbarContent'
 });
